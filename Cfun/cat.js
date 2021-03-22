c426831722ebe3027024e879ef1d1b28ffc35ce9
Crazy4k <75394354+Crazy4k@users.noreply.github.com>
2021-03-22 16:35:23 +0100

    Update cat.js

--- Cfun/cat.js
+++ Cfun/cat.js
@@ -8,7 +8,7 @@ module.exports = {
 	usage:'!cat',
 	execute(message, args) {
 		try {
-			axios.get('https://api.thecatapi.com/v1/images/search?api_key=117e183e-c93e-48f3-9dc5-fa0cd1a37350')
+			axios.get('https://api.thecatapi.com/v1/images/search?api_key= key goes here ')
 				.then((res)=>{
 					message.channel.send(`${res.data[0].url}`);
 				})
