426413b7cc8efb18cb591f6276c37362d43b646b
Crazy4k <75394354+Crazy4k@users.noreply.github.com>
2021-03-25 17:54:21 +0100

    Update avatar.js
    added a comment

--- Cothers/avatar.js
+++ Cothers/avatar.js
@@ -5,8 +5,9 @@ client.client;
 module.exports = {
 	name : 'avatar',
 	description : 'sends the avatar of the user',
-	usage:'!avatar <@user>',
+	usage:'!avatar [@user]',
 	execute(message, args) {
+		//if there was no arguments, send the avatar of the sender
 		if(!args.length) {
 			const image = message.author.displayAvatarURL();
 			message.channel.send(image);
