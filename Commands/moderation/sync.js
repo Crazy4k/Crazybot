9a29167940414ec55311d5a0ff8ee540d174a508
crazy4k <mhabbudi@gmail.com>
2021-07-11 22:07:18 +0200

    Update sync.js

--- Commands/moderation/sync.js
+++ Commands/moderation/sync.js
@@ -17,7 +17,7 @@ const mongo = require("../../mongo");
 module.exports = {
 	name : 'sync',
 	description : "syncs all data between the bot's cache and the data base, removes left members from the server's data base and creates some files for the server if missing.",
-    //cooldown: 5 * 60,
+    cooldown: 5 * 60,
     whiteList:'ADMINISTRATOR',
 	usage:'!sync',
 	category:"Moderation",
