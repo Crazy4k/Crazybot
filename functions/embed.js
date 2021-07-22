374bf2d703f8441998cce3232a6e197df762c043
crazy4k <mhabbudi@gmail.com>
2021-07-22 20:07:04 +0200

    Update embed.js

--- functions/embed.js
+++ functions/embed.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-let creator = "developed by Crazy4k";
+let creator = "developed by Crazy4k#0091";
 const moment = require("moment");
-if(moment().format("MMMM DD") === "July 02")creator = "developed by Crazy4k 🎂🎉";
+if(moment().format("MMMM DD") === "July 02")creator = "developed by Crazy4k#0091 🎂🎉";
 module.exports = (title, description, server, timestamp = false ,footer = creator, ) => {
     let color;
     if(typeof server === "object")color =server.defaultEmbedColor;
